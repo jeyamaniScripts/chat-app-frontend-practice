95b37490007139a806f16caeb41a3840b6f9aa6f
jeyamaniScripts <thejeyamani034@gmail.com>
2026-02-05 16:22:43 +0530

    socket update

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
+// export const socket = io("http://localhost:4000", {
+//   autoConnect: false,
+//   withCredentials: true,
+// });
+export const socket = io("https://chat-app-chatservice.onrender.com", {
   autoConnect: false,
   withCredentials: true,
 });
